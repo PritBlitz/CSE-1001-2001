f9149003a3845f9d396e5ba6210a0b5ecf8ad9e8
Pritish Biswas <85893461+PritBlitz@users.noreply.github.com>
2023-11-01 19:31:37 +0530

    Update icp_q1.java

--- icp_q1.java
+++ icp_q1.java
@@ -8,9 +8,9 @@ Q.1  Write a Java program to display following messages :
 public class icp_q1 {
     public static void main(String[] args) {
         System.out.println("Hello \"ITERIAN\"");
-        System.out.println("Welcome to SOA family . ");
-        System.out.println("Welcome to ICP lab . ");
-        System.out.println("Don't Worry . Java is fun for All !!!!!");
+        System.out.println("Welcome to Siksha \'O\' Anusandhan Family . ");
+        System.out.println("Welcome to \"Introduction to Computer Laboratory\" ");
+        System.out.println("Java is fun for All !!");
         
     }
 }
