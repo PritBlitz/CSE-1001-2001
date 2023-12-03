c5db8556115fe9694c74fe4d1deb1bddd71fd50d
Pritish Biswas <85893461+PritBlitz@users.noreply.github.com>
2023-12-03 20:15:33 +0530

    Add files via upload

---
+++ ICP Assisgnment 2/Assignment/icp_q1.java
@@ -0,0 +1,16 @@
+/*
+Q.1  Write a Java program to display following messages :
+        Hello "ITERIAN"
+        Welcome to SOA family .
+        Welcome to ICP Lab .
+        Don't Worry . Java is fun for All !!!!!
+ */
+public class icp_q1 {
+    public static void main(String[] args) {
+        System.out.println("Hello \"ITERIAN\"");
+        System.out.println("Welcome to SOA family . ");
+        System.out.println("Welcome to ICP lab . ");
+        System.out.println("Don't Worry . Java is fun for All !!!!!");
+        
+    }
+}
