f1cb8c2de4fd699222d92fc5500c1fc8d064ec5a
Pritish Biswas <85893461+PritBlitz@users.noreply.github.com>
2023-12-03 18:26:57 +0530

    Add files via upload

---
+++ icp_assignment_001/src/icp_q4.java
@@ -0,0 +1,11 @@
+/*
+Q.4   Write a java program that will display your first , Middle and last name in different lines without
+      using the method println() .
+ */
+
+public class icp_q4 {
+    public static void main(String[] args) {
+
+        System.out.println("Pritish \nMiddleName \nBiswas");
+    }
+}
